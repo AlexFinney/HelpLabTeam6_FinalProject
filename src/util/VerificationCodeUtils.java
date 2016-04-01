501d1dba0a3f02335726e5ba03b13d0e0eff8cbe
Alex Finney <alexander.n.finney@gmail.com>
2016-04-01 11:52:01 -0500

    Finished Code Generation

--- src/util/VerificationCodeUtils.java
+++ src/util/VerificationCodeUtils.java
@@ -1,12 +1,66 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Random;
+
+import database.MySQLAccess;
+
 public class VerificationCodeUtils {
 	
 	public static String generateCode(){
-		return "DFGCG-DGJTH-KSJDN-DISKJ-POIMN";
+		Random random = new Random(System.currentTimeMillis());
+		
+		String code = "";
+		for(int i = 0; i < 5; i++){
+			for(int j = 0; j < 5; j++){
+				char c = (char) (random.nextInt(26) + 65);
+				code += c;
+			}
+			if(i != 4)
+				code += "-";
+		}
+		
+		Connection c = MySQLAccess.getConnection();
+		
+		try {
+			System.out.println("Code is: " + code);
+			String query = "INSERT INTO codes VALUES(\'" + code + "\')";
+			PreparedStatement stmt = c.prepareStatement(query);
+			stmt.execute();
+			c.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		return code;
 	}
 	
 	public static boolean verifyCode(String code){
-		return code.equals("DFGCG-DGJTH-KSJDN-DISKJ-POIMN");
+		Connection c = MySQLAccess.getConnection();
+		
+		try {
+			String query = "SELECT * FROM codes WHERE code=?";
+			PreparedStatement stmt = c.prepareStatement(query);
+			stmt.setString(1, code);
+			
+			stmt.execute();
+			
+			ResultSet rs = stmt.getResultSet();
+			
+			boolean ret = rs.next();
+
+			c.close();
+			
+			return ret;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	
 	}
 }
